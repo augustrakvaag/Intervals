2c20a856b1294abb8391e639d13583894892139d
pearsponge <augustrakvaag@gmail.com>
2024-10-14 22:48:11 +0200

    Initial commit

---
+++ intervalcreator.py
@@ -0,0 +1,21 @@
+import time
+import eel
+
+eel.init('web')
+
+filename = "5x5.txt"
+
+@eel.expose
+def create_intervals():
+    interval_set = []
+    with open("sessions/" + filename, 'r') as file:
+        for index,line in enumerate(file):
+            if index == 0:
+                continue
+            else:
+                line = line.strip()
+                duration_power = line.split(",")
+                interval_set.append(duration_power)
+    return interval_set
+
+eel.start('index.html')
